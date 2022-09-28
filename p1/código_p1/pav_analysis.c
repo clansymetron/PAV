5350a29a0e92096041b194f71cb4c332fecf50f9
Alejandro Amat <gobberishch@gmail.com>
2022-09-28 20:53:04 +0200

    modificaciones

--- p1/código_p1/pav_analysis.c
+++ p1/código_p1/pav_analysis.c
@@ -2,33 +2,27 @@
 #include "pav_analysis.h"
 
 float compute_power(const float *x, unsigned int N) {
-    float res=0;
-    int i;
-    for(i=0;i<N;i++){
-    	res=res + ((x[i] )* x[i]);
-    }
-    res= res/N;
-    res= (float) 10*log10(res);
-    return res;
+    float res = 0;
+    for(int i = 0;i < N;i++)
+    	res += (x[i] * x[i]);
+    
+    return (float) 10*log10(res/N);
+    
 }
 
 float compute_am(const float *x, unsigned int N) {
     float res=0;
-    int i;
-    for(i=0;i<N;i++){
-    	res=res + (float) fabs((x[i] ));
-    }
-    res= res/N;
-    return res;
+    for(int i = 0 ;i < N;i++)
+    	res += (float) fabs(x[i]);
+    return res/N;
 }
 
 float compute_zcr(const float *x, unsigned int N, float fm) {
-   int i;
-   float res;
-   for(i=1;i<N;i++){
-   	if((x[i]>0 && x[i-1]<0) || (x[i]<0 && x[i-1]>0) )
-   	res=res+1;
+   float res=0;
+   for(int i =1; i < N;i++){
+   	if((x[i]>0 && x[i-1]<0) || (x[i]<0 && x[i-1]>0) ) //falta considerar caso 0
+   	    res++;
    }
-   res= res*fm/(2*(N-1));
-   return res;
+   return res*fm/(2*(N-1));
+   
 }
