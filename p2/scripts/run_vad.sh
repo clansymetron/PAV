564c47306e3c5c8413a88f2c251fb1266d31199e
Alejandro Amat <gobberishch@gmail.com>
2022-10-24 17:28:45 +0200

    All files

--- p2/scripts/run_vad.sh
+++ p2/scripts/run_vad.sh
@@ -2,11 +2,14 @@
 
 # Be sure that this file has execution permissions:
 # Use the nautilus explorer or chmod +x run_vad.sh
+#$alfa1 = 13;
 
 # Write here the name and path of your program and database
-DIR_P2=$HOME/PAV/P2
+DIR_P2=$HOME/Desktop/CFIS/4A/PAV/practicas/PAV/p2
 DB=$DIR_P2/db.v4
-CMD="$DIR_P2/bin/vad --alfa=5"
+
+
+CMD="$DIR_P2/src/p2 --alfa1=3.53 --alfa2=5.01"
 
 for filewav in $DB/*/*wav; do
 #    echo
@@ -26,6 +29,6 @@ for filewav in $DB/*/*wav; do
 
 done
 
-scripts/vad_evaluation.pl $DB/*/*lab
+../scripts/vad_evaluation.pl $DB/*/*lab
 
 exit 0
